456269c3f65259ad3583568d6bbfcab7d7ac5c7b
Mark <34418187+markveligod@users.noreply.github.com>
2022-06-11 19:22:15 +0300

    Update ALS and Interact
    - Добавил интеракт компонент
    - изменил пресеты карты
    - добавил логи в ALS

---
+++ Source/RisingSignal/Game/InteractSystem/InteractComponent.h
@@ -0,0 +1,47 @@
+// It is owned by the company Dverg Verksted.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InteractComponent.generated.h"
+
+class UBoxComponent;
+class ARSGamePLayer;
+
+/**
+ * @class
+ **/
+UCLASS(ClassGroup = (Managers), meta = (BlueprintSpawnableComponent),
+	HideCategories = ("Variable", "Transform", "Sockets", "Shape", "Navigation", "ComponentTick", "Physics", "Tags", "Cooking", "HLOD",
+		"Mobile", "Activation", "Component Replication", "Events", "Asset User Data", "Collision"))
+class RISINGSIGNAL_API UInteractComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UInteractComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+#if WITH_EDITOR
+	//
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+private:
+	// @private Size box collision
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Settings Interact", meta = (AllowPrivateAccess = true))
+	FVector SizeBoxCollision = FVector(50.f);
+
+	// @private Owner component
+	UPROPERTY()
+	ARSGamePLayer* OwnerPlayer;
+
+	// @private Box component Collision
+	UPROPERTY()
+	UBoxComponent* BoxCollision;
+};
