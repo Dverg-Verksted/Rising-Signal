3d9cb69d331ffd851869e227f270b04e5e9cbcfd
Mark <34418187+markveligod@users.noreply.github.com>
2022-07-03 10:34:19 +0500

    Fix ALS
    - Адаптировал под свой класс ALS

--- Source/RisingSignal/RisingSignal.Build.cs
+++ Source/RisingSignal/RisingSignal.Build.cs
@@ -10,7 +10,7 @@ public class RisingSignal : ModuleRules
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PhysicsCore" });
 
-        PrivateDependencyModuleNames.AddRange(new string[] {});
+        PrivateDependencyModuleNames.AddRange(new string[] { "ALS", "ALSCamera" });
 
         PublicIncludePaths.AddRange(new string[] { "RisingSignal" });
 
