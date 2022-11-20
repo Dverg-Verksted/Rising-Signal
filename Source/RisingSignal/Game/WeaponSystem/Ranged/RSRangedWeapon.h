4c2078414169e96110c57305c674b91ca82d030b
Petr Gagarskiy <vacumsio@gmail.com>
2022-11-20 17:58:35 +0300

    Ammo logic goes to InventoreSystem

--- Source/RisingSignal/Game/WeaponSystem/Ranged/RSRangedWeapon.h
+++ Source/RisingSignal/Game/WeaponSystem/Ranged/RSRangedWeapon.h
@@ -6,45 +6,21 @@
 #include "Game/WeaponSystem/RSBaseWeapon.h"
 #include "RSRangedWeapon.generated.h"
 
-DECLARE_MULTICAST_DELEGATE(FOnClipEmptySignature);
-
-USTRUCT(BlueprintType)
-struct FAmmoData
-{
-    GENERATED_USTRUCT_BODY();
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Оружие")
-    int32 Bullets;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Оружие", meta=(EditCondition = "!Infinite"))
-    int32 Clips;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Оружие")
-    bool Infinite;
-};
-
 UCLASS()
 class RISINGSIGNAL_API ARSRangedWeapon : public ARSBaseWeapon
 {
 	GENERATED_BODY()
 
 public:
-    FOnClipEmptySignature FOnClipEmpty;
-
     virtual void StartAiming();
     virtual void StopAiming();
-    
     virtual void StartAttack();
     virtual void StopAttack();
-
+    
     void ChangeClip();
     bool CanReload() const;
 
 protected:
-    
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Оружие")
-    FAmmoData DefaultAmmo{15,10,false};
-
     virtual void BeginPlay() override;
     virtual void MakeShot();
     virtual bool GetTraceData(FVector& TraceStart, FVector& TraceEnd) const override;
@@ -53,7 +29,4 @@ protected:
     bool IsAmmoEmpty() const;
     bool IsClipEmpty() const;
     void LogAmmo();
-
-private:
-    FAmmoData CurrentAmmo;
 };
